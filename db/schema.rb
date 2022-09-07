aabe52a4c2f35bf5e5974225a7e527a6a9b0d91a
leroy-netizen <leroythomas051@gmail.com>
2022-09-08 00:04:29 +0300

    run migrations for user table and generate a schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_200744) do
+ActiveRecord::Schema.define(version: 2022_09_07_205533) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding starts..."
+
+
+puts "seeding users table...."
+single_user1 = User.create(first_name: "Leroy", last_name: "Ombiji", username: "Baba wa keja",password:"12345678")
+single_user2 = User.create(first_name: "Thomas", last_name: "Odori", username: "Sasco",password:"12345678")
 
 # Seed your database here
 
